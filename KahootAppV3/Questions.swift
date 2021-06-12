72b33a9dcc96f8e042bca3b16d5ce0f7b9528420
uncoolbeans <ivcleandedmyroom@gmail.com>
2021-06-12 11:09:14 +0800

    Inital commit

---
+++ KahootAppV3/Questions.swift
@@ -0,0 +1,17 @@
+//
+//  Questions.swift
+//  KahootAppV3
+//
+//  Created by MMS on 12/6/21.
+//
+
+import Foundation
+struct Question {
+    
+    var title: String
+    
+    var option1: String
+    var option2: String
+    var option3: String
+    var option4: String
+}
---
+++ KahootAppV3/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  KahootAppV3
+//
+//  Created by MMS on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
