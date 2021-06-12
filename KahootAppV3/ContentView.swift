12f7e8f4646b2bd012ad7143486c9dafaa94cd49
uncoolbeans <ivcleandedmyroom@gmail.com>
2021-06-12 11:48:54 +0800

    Added 4th question to array.

--- KahootAppV3/ContentView.swift
+++ KahootAppV3/ContentView.swift
@@ -20,7 +20,8 @@ struct ContentView: View {
     var questions = [
         Question(title: "What day of the week is it?", option1: "Monday", option2: "Saturday", option3: "Sunday", option4: "Wednesday"),
         Question(title: "What does YJ eat?", option1: "Broccoli", option2: "Nutella", option3: "Rice", option4: "MacBooks"),
-        Question(title: "Did Jia Chen sleep last night?", option1: "Yes", option2: "No", option3: "Yes but I needed another answer", option4: "Yes?")
+        Question(title: "Did Jia Chen sleep last night?", option1: "Yes", option2: "No", option3: "Yes but I needed another answer", option4: "Yes?"),
+        Question(title: "What is the offical questions channel?", option1: "#text-questions",option2: "ed",option3: "Webex",option4: "nothing, no questions")
     ]
     
     var body: some View {
